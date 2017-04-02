c7145fad1eefe5bf93214e1d84add376a366c1b8
Giorgio Micotti <giorgiomicotti@gmail.com>
2017-04-02 13:06:41 +0200

    Create common device tree for msm8909 devices

---
+++ board/00-qcom-platform-msm8909.mk
@@ -0,0 +1,20 @@
+# Platform
+TARGET_BOARD_PLATFORM := msm8909
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a7
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_msm8909
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8909
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QC_TIME_SERVICES := true
+TARGET_POWERHAL_VARIANT := qcom
+TARGET_RIL_VARIANT := caf
--- init/init_msm8909.cpp
+++ init/init_msm8909.cpp
@@ -39,7 +39,7 @@
 #include "log.h"
 #include "util.h"
 
-#include "init_msm8916.h"
+#include "init_msm8909.h"
 
 __attribute__ ((weak))
 void init_target_properties()
