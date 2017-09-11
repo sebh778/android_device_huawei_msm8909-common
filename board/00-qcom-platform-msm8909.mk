1c522569a61ba55b0d1f98c0f55a3f9e5bc0fad2
desalesouche <desalesouche@gmail.com>
2017-09-11 02:21:51 +0200

    more update from lineageos huawei msm8916
    Change-Id: Id16e74e30557b1c3ca4e5385ec2fc042358c0771

--- board/00-qcom-platform-msm8909.mk
+++ board/00-qcom-platform-msm8909.mk
@@ -10,6 +10,9 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a7
 
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=softfp
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8909
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -5,6 +5,6 @@ PRODUCT_PACKAGES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.core_ctl_min_cpu=0 \
+    ro.core_ctl_min_cpu=2 \
     ro.core_ctl_max_cpu=4 \
     ro.min_freq_0=800000
